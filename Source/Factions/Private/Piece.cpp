72704097e72c2f8103f6548219a001853f8b1127
Addison <addyshuppy@gmail.com>
2025-08-05 20:06:38 -0600

    Got code to compile using VSCode

---
+++ Source/Factions/Private/Piece.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Piece.h"
+
+// Sets default values
+APiece::APiece()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APiece::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APiece::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APiece::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+

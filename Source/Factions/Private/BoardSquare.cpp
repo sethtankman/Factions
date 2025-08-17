1e2f18f7d9eb2c1a2161f37a251c0850946a84d0
Addison <addyshuppy@gmail.com>
2025-08-16 22:54:41 -0600

    Figured out mouse-over events, Brought in a pawn

---
+++ Source/Factions/Private/BoardSquare.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BoardSquare.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+ABoardSquare::ABoardSquare()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	// Our root component will be a sphere that reacts to physics
+	UBoxComponent* BoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("RootComponent"));
+    RootComponent = BoxComponent;
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	if(Mesh) {
+		Mesh->SetupAttachment(RootComponent);
+		Mesh->OnBeginCursorOver.AddDynamic(this, &ABoardSquare::BeginMouseOver);
+		Mesh->OnEndCursorOver.AddDynamic(this, &ABoardSquare::EndMouseOver);
+	}
+}
+
+// Called when the game starts or when spawned
+void ABoardSquare::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ABoardSquare::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ABoardSquare::BeginMouseOver(UPrimitiveComponent *TouchedComponent)
+{
+	SetActorHiddenInGame(true);
+}
+
+void ABoardSquare::EndMouseOver(UPrimitiveComponent *TouchedComponent)
+{
+	SetActorHiddenInGame(false);
+}
---
+++ Source/Factions/Public/BoardSquare.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BoardSquare.generated.h"
+
+UCLASS()
+class FACTIONS_API ABoardSquare : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABoardSquare();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere) 
+	UStaticMeshComponent* Mesh;
+
+	UFUNCTION()
+	void BeginMouseOver(UPrimitiveComponent* TouchedComponent);
+
+	UFUNCTION()
+	void EndMouseOver(UPrimitiveComponent* TouchedComponent);
+
+};
